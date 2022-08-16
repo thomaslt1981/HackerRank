fe30d5d7891e3b5834db843708a51ce2c5c6677d
thomaslt1981 <93420651+thomaslt1981@users.noreply.github.com>
2022-08-16 14:23:59 +0800

    Update FurnitureOrder.java

--- FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
+++ FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -6,7 +6,7 @@ public class FurnitureOrder implements FurnitureOrderInterface {
     /**
      * TODO: Create a map of Furniture items to order quantities
      */
-
+/*88*/
     /**
      * Initialize a new mapping of Furniture types to order quantities.
      */
